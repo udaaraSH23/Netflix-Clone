6657101d055c844612b50792917d581b2a4fff0c
udaaraSH23 <senarat-ec20001@stu.kln.ac.lk>
2025-04-28 17:30:42 +0530

    Added Global Exception Handler

---
+++ server-springboot/netflix-backend-springboot/src/main/java/com/example/netflix_backend_springboot/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.netflix_backend_springboot.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        body.put("details", request.getDescription(false));
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
